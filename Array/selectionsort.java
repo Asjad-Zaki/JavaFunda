65e2dab973b523d4020b02dc7d226de3a9525f55
Asjad-Zaki <93717246+Asjad-Zaki@users.noreply.github.com>
2023-09-21 07:28:08 +0530

    Add files via upload

---
+++ Array/selectionsort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class selectionsort{
+    public static void selctionsort(int arr[]){ 
+        for(int i=0;i<arr.length-1;i++){
+            int minpos=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minpos]<arr[j]){
+                    minpos=j;
+                }
+            }
+             //swap
+            int temp=arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={5,4,1,3,2};
+        selctionsort(arr);
+
+    }
+}
